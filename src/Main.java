23a1ff0ec2a94d0b45f3e2293462d6401eb67641
Team 31 AM: 31442, 34538, 32903 <ianagnostopoyloy@yahoo.com>
2013-02-11 23:07:12 +0200

    ο κώδικας με τις απαιτήσεις

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+
+
+public static void main(String[] args) {
+      UsersForm users=new UsersForm();
+      users.setVisible(true);
+      
+      }
+
+}
---
+++ src/Show.java
@@ -0,0 +1,25 @@
+public class Show {
+    private String name;
+    private String date;
+    
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    
+    public String getName()
+    {
+         return name;
+    }
+    
+    public void setDate(String date)
+    {
+        this.date = date;
+    }
+    
+    public String getDate()
+    {
+         return date;
+    }
+    
+}
